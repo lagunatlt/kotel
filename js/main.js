ecdb972ac294de4c77fd2062bd340294006c35ed
lagunatlt <lagunatlt@yandex.ru>
2020-04-12 17:20:59 +0400

    ok

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
-12
+
+/* запрет скролла google maps */
+$(document).ready(function () {
+	$('#map1').addClass('scrolloff');
+	$('#overlay').on('click', function () {
+		$('#map1').removeClass('scrolloff');
+	});
+	$("#map1").mouseleave(function () {
+		$('#map1').addClass('scrolloff');
+	});
+});
+/* -------------------------- */
